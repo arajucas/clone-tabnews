eea3a73322f75719b9f3eedaeb58743ab626bce6
arajucas <140090828+arajucas@users.noreply.github.com>
2023-08-16 20:52:55 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação de um clone do tabnews.com.br para fins de estudo.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>esposa minha, se você me ama, dá uma risadinha :D </h1>
+  return <h1>esposa minha, se você me ama, dá uma risadinha :D </h1>;
 }
 
 export default Home;
